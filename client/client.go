ab3ed7b8b35c868852a6e2aa84fcb368553490c0
Froggy <froggy.happy.f@gmail.com>
2021-12-20 00:51:56 +0300

    Add client and server

---
+++ client/client.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"github.com/XFroggyX/chat-with-doubleratchet/encodeCharset"
+	"io"
+	"log"
+	"net"
+	"os"
+)
+
+const (
+	connectType = "tcp"
+	connectPort = "3333"
+	connectHost = "0.0.0.0"
+)
+
+func main() {
+	tcpAddr, err := net.ResolveTCPAddr(connectType, connectHost+":"+connectPort)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	conn, err := net.DialTCP("tcp", nil, tcpAddr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+	go printOutput(conn)
+	writeInput(conn)
+}
+
+func writeInput(conn *net.TCPConn) {
+	fmt.Print("Enter username: ")
+	reader := bufio.NewReader(os.Stdin)
+	username, err := reader.ReadString('\n')
+	username = username[:len(username)-1]
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Enter text: ")
+	for {
+		text, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+		err = common.WriteMsg(conn, username+": "+text)
+		if err != nil {
+			log.Println(err)
+		}
+	}
+}
+
+func printOutput(conn *net.TCPConn) {
+	for {
+
+		msg, err := common.ReadMsg(conn)
+		if err == io.EOF {
+			conn.Close()
+			fmt.Println("Connection Closed. Bye bye.")
+			os.Exit(0)
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(msg)
+	}
+}
---
+++ server/server.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+	"github.com/XFroggyX/chat-with-doubleratchet/encodeCharset"
+	"io"
+	"log"
+	"net"
+	"os"
+)
+
+const (
+	connectType = "tcp"
+	connectPort = "3333"
+	connectHost = "0.0.0.0"
+)
+
+var connections []net.Conn
+
+func main() {
+	l, err := net.Listen(connectType, connectHost+":"+connectPort)
+	if err != nil {
+		log.Panicln("Error listening: ", err.Error())
+		os.Exit(1)
+	}
+
+	defer l.Close()
+	log.Println("Listening on " + connectHost + ":" + connectPort)
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Panicln("Error accepting: ", err.Error())
+			os.Exit(1)
+		}
+		connections = append(connections, conn)
+		go handleRequest(conn)
+	}
+
+}
+
+func handleRequest(conn net.Conn) {
+	for {
+		msg, err := common.ReadMsg(conn)
+		if err != nil {
+			if err == io.EOF {
+				removeConn(conn)
+				conn.Close()
+				return
+			}
+			log.Println(err)
+			return
+		}
+		fmt.Printf("Message Received: %s\n", msg)
+		broadcast(conn, msg)
+	}
+}
+
+func removeConn(conn net.Conn) {
+	var i int
+	for i := range connections {
+		if connections[i] == conn {
+			break
+		}
+	}
+	connections = append(connections[:i], connections[i+1:]...)
+}
+
+func broadcast(conn net.Conn, msg string) {
+	for i := range connections {
+		if connections[i] != conn {
+			err := common.WriteMsg(connections[i], msg)
+			if err != nil {
+				log.Println(err)
+			}
+		}
+	}
+}
