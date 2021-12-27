c0b06e7670e6f9c01788130709aab7981ac1a1ba
Froggy <froggy.happy.f@gmail.com>
2021-12-27 16:18:39 +0300

    Add encodeCharset, который использую для котроля контроля размера соббщение

--- keysPair/keysPair.go
+++ keysPair/keysPair.go
@@ -1,15 +1,15 @@
-package keysPair
+package KeysPair
 
 import "github.com/farazdagi/x3dh"
 
-type keysPair struct {
+type KeysPair struct {
 	curve      x3dh.Curve25519
 	publicKey  x3dh.PublicKey
 	privateKey x3dh.PrivateKey
 	sharedKey  []byte
 }
 
-func (k *keysPair) generatingKeyPair() {
+func (k *KeysPair) generatingKeyPair() {
 	k.curve = x3dh.NewCurve25519()
 	k.privateKey, _ = k.curve.GenerateKey(nil)
 	k.publicKey = k.curve.PublicKey(k.privateKey)
